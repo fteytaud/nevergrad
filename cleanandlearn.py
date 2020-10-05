ab1f5a2cd453a9e5a74270ed3c26c90adbada2b0
Fabien Teytaud <teytaud@univ-littoral.fr>
2020-10-05 11:40:50 +0200

    rerun

--- cleanandlearn.py
+++ cleanandlearn.py
@@ -17,11 +17,28 @@ import torch.nn.functional as F
 from torch import optim
 import sys
 import getopt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import tree
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.naive_bayes import GaussianNB
+import keras
 min_max_scaler = preprocessing.MinMaxScaler()
 
 
-def keepBest(data):
+def keepBest(data, k=0):
     data = data[['dimension','budget', 'loss','optimizer_name']]
     new = data["optimizer_name"].str.split("_", n = 2, expand = True)
     data["mu"] = new[1] 
@@ -42,7 +59,12 @@ def keepBest(data):
         tmp = data[data['dimension'] == d]
         tmp = tmp[tmp['budget'] == b]
         tmp = tmp[tmp['lambda'] == l]
-        themin = tmp[tmp.loss == tmp.loss.min()]
+        tmp2 = tmp['loss']
+        print(tmp2.shape)
+        tmp2 = tmp2.sort_values(ascending=True)
+        tmp2 = tmp2.reset_index(drop=True)
+        v_min = tmp2[k]
+        themin = tmp[tmp.loss <= v_min]
         cleaned_data = cleaned_data.append(themin)
     del(cleaned_data['loss'])
     cleaned_data = cleaned_data.astype(dtype=float)
@@ -58,7 +80,6 @@ def analyze(data):
     print(data.info())
     print(data.describe())
     print(data.head())
-    data.to_csv('prout.csv')
     # data.plot.scatter(x='mu', y='lambda');
     # plt.show()
 
@@ -66,13 +87,47 @@ def analyze(data):
     yy = 'lambda'
     zz = 'dimension'
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, projection='3d')
-    ax1.scatter(data[xx], data[yy], data[zz])
-    ax1.set_xlabel(xx)
-    ax1.set_ylabel(yy)
-    ax1.set_zlabel(zz)
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(111, projection='3d')
+    # ax1.scatter(data[xx], data[yy], data[zz])
+    # ax1.set_xlabel(xx)
+    # ax1.set_ylabel(yy)
+    # ax1.set_zlabel(zz)
+    # plt.show()
+
+    # dimension = 2
+    # datatmp = data[data["dimension"]==dimension]
+    # for budget in [30, 50, 100, 300, 600, 900, 1500, 2000, 2500, 3000]:
+    #     data2 = datatmp[datatmp["budget"]==budget]
+    #     plt.plot(data2[yy], np.log(data2[xx]), label='budget={}'.format(budget))
+    #     print("Budget {} : {} : {}".format(budget, data2[yy], data2[xx]))
+    # plt.legend()
+    # plt.show()
+
+    # dimension = 2
+    # datatmp = data[data["dimension"]==dimension]
+    # for budget in [30, 50, 100, 300, 600, 900, 1500, 2000, 2500, 3000]:
+    #     data2 = datatmp[datatmp["budget"]==budget]
+    #     plt.boxplot(data2[yy], np.log(data2[xx]))
+    #     print("Budget {} : {} : {}".format(budget, data2[yy], data2[xx]))
+    # legend = ['budget={}'.format(budget) for budget in [30, 50, 100, 300, 600, 900, 1500, 2000, 2500, 3000]]
+    # plt.legend(legend)
+    # plt.show()
+
+    dimension = 2
+    datatmp = data[data["dimension"]==dimension]
+    lambdas = np.unique(data[['lambda']], axis=0)
+    data2 = datatmp[datatmp["budget"]==30]
+    data2 = data2.drop(['dimension', 'budget'], axis=1)
+    lambdas = lambdas.reshape(-1)
+    for l in lambdas:
+        data3 = data2[data2['lambda']==l].copy()
+        data3 = data3['mu']#.drop(['lambda'], axis=1)
+        plt.boxplot(data3, positions=[l])
     plt.show()
+
+    data.to_csv('prout.csv')
+    data2.to_csv('prout2.csv')
 
 
 def splitData(data, test_ratio):
@@ -102,54 +157,27 @@ def display_score(classifier, x_train, y_train, x_test, y_test):
     print('MSE: %s' % mean_squared_error(y_test, y_pred))
 
 
-###############################################
-###### With pytorch
-###############################################
-class Model(nn.Module):
-    def __init__(self, ):
-        super().__init__()
-        self.inputSize = 3
-        self.outputSize = 1
-        # self.hiddenSize1 = 30
-        # self.hiddenSize2 = 80
-        # self.hiddenSize3 = 20
-        # self.hiddenSize4 = 5
-        
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(self.inputSize, self.hiddenSize1),
-        #     # nn.BatchNorm1d(self.hiddenSize1),
-        #     nn.Dropout(0.3),
-        #     nn.ReLU(),
-        #     nn.Linear(self.hiddenSize1, self.hiddenSize2),
-        #     # nn.BatchNorm1d(self.hiddenSize2),
-        #     nn.Dropout(0.3),
-        #     nn.ReLU(),
-        #     nn.Linear(self.hiddenSize2, self.hiddenSize3),
-        #     nn.Dropout(0.3),
-        #     nn.ReLU(),
-        #     nn.Linear(self.hiddenSize3, self.hiddenSize4),
-        #     nn.Dropout(0.3),
-        #     nn.ReLU(),
-        #     nn.Linear(self.hiddenSize4, self.outputSize)
-        # )
-        
-        self.hiddenSize1 = 10
-        self.hiddenSize2 = 10
-        self.classifier = nn.Sequential(
-            nn.Linear(self.inputSize, self.hiddenSize1),
-            # nn.BatchNorm1d(self.hiddenSize1),
-            nn.Dropout(0.3),
-            nn.ReLU(),
-            nn.Linear(self.hiddenSize1, self.hiddenSize2),
-            # nn.BatchNorm1d(self.hiddenSize1),
-            nn.Dropout(0.3),
-            nn.ReLU(),
-            nn.Linear(self.hiddenSize2, self.outputSize)
-        )
 
-    def forward(self, x):
-        x = self.classifier(x)
-        return x
+def batch_classify(_dict_classifiers, _X_train, _Y_train, _X_test, _Y_test, verbose=True):
+    # Apprentissage sur un dictionnaire de classifieurs
+    df_results = pd.DataFrame(data=np.zeros(shape=(len(_dict_classifiers.keys()), 4)),
+                              columns=['classifier', 'train_score', 'test_score', 'training_time'])
+    count = 0
+    for key, classifier in _dict_classifiers.items():
+        time_taken = time.perf_counter()
+        classifier.fit(_X_train, _Y_train)
+        time_taken = time.perf_counter() - time_taken
+        train_score = classifier.score(_X_train, _Y_train)
+        test_score = classifier.score(_X_test, _Y_test)
+        df_results.loc[count, 'classifier'] = key
+        df_results.loc[count, 'train_score'] = train_score
+        df_results.loc[count, 'test_score'] = test_score
+        df_results.loc[count, 'training_time'] = time_taken
+        if verbose:
+            print("Trained {c} in {f:.2f} s".format(c=key, f=time_taken))
+        count += 1
+
+    return df_results
 
 def usage():
     print("Usage: ./cleanandlearn --filename=data_file_name.csv")
@@ -170,61 +198,28 @@ def main(argv):
 
 
     data = pd.read_csv(filename, sep=',')
-    cleaned_data = keepBest(data)
+    cleaned_data = keepBest(data, 50)
     analyze(cleaned_data)
     return
+
     x_train, y_train, x_test, y_test = splitData(cleaned_data, 0.2)
     print(x_train.shape)
     print(x_test.shape)
-    model = Model()
-    loss_function = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters())
-    # optimizer = optim.RMSprop(model.parameters())
-    # optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay= 1e-6, momentum = 0.9, nesterov = True)
+    # Dictionnaire des classifieurs que l'on souhaite tester
+    dict_classifiers = {
+        "Logistic Regression": LogisticRegression(),
+        "Nearest Neighbors": KNeighborsRegressor(),
+        "Linear SVM": SVC(),
+        "Gradient Boosting Regressor": GradientBoostingRegressor(),
+        "Decision Tree": tree.DecisionTreeRegressor(),
+        "Random Forest": RandomForestRegressor(n_estimators=10000, max_depth=6),
+        "Neural Net": MLPRegressor(alpha=1),
+        "Naive Bayes": GaussianNB()
+    }
 
+    results = batch_classify(dict_classifiers, x_train, y_train, x_test, y_test)
+    print(results.sort_values(by='test_score', ascending=False))
 
-    data_train = torch.utils.data.TensorDataset(torch.Tensor(np.array(x_train)), torch.Tensor(np.array(y_train)))
-    train_loader = torch.utils.data.DataLoader(data_train, batch_size = 64, shuffle = True)
-
-    data_test = torch.utils.data.TensorDataset(torch.Tensor(np.array(x_test)), torch.Tensor(np.array(y_test)))
-    test_loader = torch.utils.data.DataLoader(data_test)
-
-    for epoch in range(1, 10001): ## run the model for 10 epochs
-        train_loss, valid_loss = [], []
-
-        ## training part 
-        model.train()
-        for data, target in train_loader:
-            optimizer.zero_grad()
-
-            ## 1. forward propagation
-            output = model(data)
-            ## 2. loss calculation
-            loss = loss_function(output, target.view(-1,1))
-            ## 3. backward propagation
-            loss.backward()
-            ## 4. weight optimization
-            optimizer.step()
-
-            train_loss.append(loss.item())
-
-        ## evaluation part 
-        model.eval()
-        for data, target in test_loader:
-            output = model(data)
-            loss = loss_function(output, target.view(-1,1))
-            valid_loss.append(loss.item())
-
-        print ("Epoch:", epoch, "Training Loss: ", np.mean(train_loss), "Valid Loss: ", np.mean(valid_loss))
-    model.eval()
-    for data, target in test_loader:
-        pred = model(data)
-        pred2 = pred.detach().numpy()
-        target2 = target.detach().numpy()
-        # pred2 = min_max_scaler.inverse_transform(pred2)
-        # target2 = min_max_scaler.inverse_transform(target2)
-        print(pred2)
-        print(target2)
 
 
 if __name__ == '__main__':
